5f3f2090216b370e044df9a4f5cf2aea02099fcf
burnpiro <kemalpiro@gmail.com>
2020-10-17 01:46:23 +0200

    build

---
+++ precache-manifest.e502e393222563fc03681fb6d6168980.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73ec16273826e92a53dffad77714fee",
+    "url": "/wod-bike-dataset-generator/index.html"
+  },
+  {
+    "revision": "7e6a425d25ba604bc992",
+    "url": "/wod-bike-dataset-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7e9df5347d728993574",
+    "url": "/wod-bike-dataset-generator/static/js/2.aecd777b.chunk.js"
+  },
+  {
+    "revision": "5333e23a7ebfa4cdc1ece3d6d2963c12",
+    "url": "/wod-bike-dataset-generator/static/js/2.aecd777b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6a425d25ba604bc992",
+    "url": "/wod-bike-dataset-generator/static/js/main.7038d7fb.chunk.js"
+  },
+  {
+    "revision": "c3ac19e07d3aeda0f948",
+    "url": "/wod-bike-dataset-generator/static/js/runtime-main.ecbcacda.js"
+  },
+  {
+    "revision": "cb327131d1962706b20036b357356a0c",
+    "url": "/wod-bike-dataset-generator/static/media/path-sample.cb327131.png"
+  },
+  {
+    "revision": "cf2f4cd5e2daf4538c2f61d77d3976f1",
+    "url": "/wod-bike-dataset-generator/static/media/station-sample.cf2f4cd5.png"
+  },
+  {
+    "revision": "1c32ff4c9b8dca2cdcab1cce29a0e713",
+    "url": "/wod-bike-dataset-generator/static/media/twitter.1c32ff4c.png"
+  }
+]);
